248ea42cd5daf9ee83faae0c9caefbc65d9d2418
YKWANG <1821370850@qq.com>
2024-05-29 17:18:20 -0400

    modify graph.py

--- noisy-llm/eval_scripts/graph.py
+++ noisy-llm/eval_scripts/graph.py
@@ -16,44 +16,47 @@ exact_match = {}
 f1 = {}
 
 
+def draw_pics(model_name_str, model_out_path):
+    exact_match[model_name_str] = {}
+    f1[model_name_str] = {}
+    for k in prompt_level_list:
+        exact_match[model_name_str][k] = {}
+        f1[model_name_str][k] = {}
+        for i in range(len(noise_level_list)):
+            file_name = model_name_str + '_noise' +  file_noise_level_str[i] + '_b16_qa_'+ str(k)+ 'prompt'
+            cur_path =  os.path.join(model_out_path, file_name, 'results.json')
+            
+            with open(cur_path, 'r') as f:
+                data = json.load(f)
+                exact_match[model_name_str][k][noise_level_list[i]] = data['exact_match']
+                f1[model_name_str][k][noise_level_list[i]] = data['f1']
 
-#vicuna
-exact_match['vicuna'] = {}
-f1['vicuna'] = {}
-for k in prompt_level_list:
-    exact_match['vicuna'][k] = {}
-    f1['vicuna'][k] = {}
-    for i in range(len(noise_level_list)):
-        file_name = 'vicuna_noise' + file_noise_level_str[i] + '_b16_qa_'+ str(k)+ 'prompt'
-        cur_path = combined_path = os.path.join(vicuna_out_path, file_name, 'results.json')
-        
-        with open(cur_path, 'r') as f:
-            data = json.load(f)
-            exact_match['vicuna'][k][noise_level_list[i]] = data['exact_match']
-            f1['vicuna'][k][noise_level_list[i]] = data['f1']
-
-# print(exact_match)
-# print(f1)
-
-PRINT_VICUNA = True
-
-if PRINT_VICUNA:
     for i in prompt_level_list:
-        exact_match_values = list(exact_match['vicuna'][i].values())
-        plt.plot(noise_level_list, exact_match_values, label=f'{i}shots', marker='o')
+        exact_match_values = list(exact_match[model_name_str][i].values())
+        plt.plot(noise_level_list, exact_match_values, label=f'{i} shots', marker='o')
         plt.legend()
-        plt.title(f'few-shots vicuna')
+        plt.title(f'few-shots {model_name_str}')
         plt.xlabel('noise_level')
         plt.ylabel('exact match score')
-    plt.savefig(f'./pics/vicuna_exact_match.png')
+    plt.savefig(f'./pics/{model_name_str}_exact_match.png')
     plt.clf()
 
     for i in prompt_level_list:
-        f1_values = list(f1['vicuna'][i].values())
-        plt.plot(noise_level_list, f1_values, label=f'{i}shots', marker='o')
+        # f1_values = list(map(lambda x: x*100,  f1[model_name_str][i].values()))
+        f1_values = list( f1[model_name_str][i].values())
+        plt.plot(noise_level_list, f1_values, label=f'{i} shots', marker='o')
         plt.legend()
-        plt.title(f'few-shots vicuna')
+        plt.title(f'few-shots {model_name_str}')
         plt.xlabel('noise_level')
         plt.ylabel('f1 score')
-    plt.savefig(f'./pics/vicuna_f1.png')
+    plt.savefig(f'./pics/{model_name_str}_f1.png')
     plt.clf()
+
+
+model_name_list = ['vicuna', 'llama_chat']
+model_out_path_list = [vicuna_out_path, llama_chat_out_path]
+for i,name in enumerate(model_name_list):
+    draw_pics(name, model_out_path_list[i])
+
+print(exact_match)
+print(f1)
